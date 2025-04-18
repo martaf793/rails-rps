300fa338a2f5564e4c539c480f8a150963510b9a
martaf793 <martaf@uchicago.edu>
2025-04-18 01:27:02 +0000

    paper

--- app/controllers/concerns/rps_controller.rb
+++ app/controllers/concerns/rps_controller.rb
@@ -14,8 +14,18 @@ class RpsController < ApplicationController
     render({:template => "game_templates/play_rock"}) 
   end
 
-  # def paper_a
-  # end
+  def paper_a
+    moves=["rock", "paper", "scissors"]
+    @computer_play=moves.sample
+    if @computer_play=="paper"
+      @outcome="tied"
+    elsif @computer_play=="scissors"
+      @outcome="lost"
+    else 
+      @outcome="won"
+    end
+    render({:template => "game_templates/play_paper"}) 
+  end
   # def scissors_a
   # end
   
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # get("/", {:controller=> "rps", :action=> "rules"})
   get("/rock", {:controller=> "rps", :action=> "rock_a"})
 
-#   get("/paper", {:controller=> "rps", :action=> "paper_a"})
+  get("/paper", {:controller=> "rps", :action=> "paper_a"})
 #   get("/scissors", {:controller=> "rps", :action=> "scissors_a"})
 end
