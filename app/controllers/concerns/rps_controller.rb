678e646b8d3ac277d5e93aff48d80f5503ef3fc4
martaf793 <martaf@uchicago.edu>
2025-04-18 01:31:30 +0000

    done

--- app/controllers/concerns/rps_controller.rb
+++ app/controllers/concerns/rps_controller.rb
@@ -1,6 +1,9 @@
 class RpsController < ApplicationController
-  # def rules
-  # end
+  def rules
+    render({:template => "game_templates/home"}) 
+
+  end
+
   def rock_a
     moves=["rock", "paper", "scissors"]
     @computer_play=moves.sample
@@ -26,7 +29,18 @@ class RpsController < ApplicationController
     end
     render({:template => "game_templates/play_paper"}) 
   end
-  # def scissors_a
-  # end
+
+  def scissors_a
+    moves=["rock", "paper", "scissors"]
+    @computer_play=moves.sample
+    if @computer_play=="scissors"
+      @outcome="tied"
+    elsif @computer_play=="paper"
+      @outcome="won"
+    else 
+      @outcome="lost"
+    end
+    render({:template => "game_templates/play_scissors"})
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # get("/", {:controller=> "rps", :action=> "rules"})
+  get("/", {:controller=> "rps", :action=> "rules"})
   get("/rock", {:controller=> "rps", :action=> "rock_a"})
 
   get("/paper", {:controller=> "rps", :action=> "paper_a"})
-#   get("/scissors", {:controller=> "rps", :action=> "scissors_a"})
+  get("/scissors", {:controller=> "rps", :action=> "scissors_a"})
 end
